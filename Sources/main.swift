55fe1a7870fb3dbe388a5a448f394eee4474774b
Ryan Poolos <ryanpoolos@Gmail.com>
2016-02-23 19:23:10 -0500

    Playing with routing.

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,6 +5,15 @@ import Inquiline
 import Curassow
 
 
-serve { _ in
-  return Response(.Ok, contentType: "text/plain", body: "Hello Peter. Looking Erect.")
+serve { request in
+    
+    if request.path == "/hard" {
+        return Response(.Ok, contentType: "text/plain", body: "Hello Peter. Looking Erect.")
+    }
+    else if request.path == "/soft" {
+        return Response(.Ok, contentType: "text/plain", body: "Hello Peter. Looking Blue.")
+    }
+    else {
+        return Response(.Ok, contentType: "text/plain", body: "Hello Peter. <a href='/hard'>I'm Hard</a>. <a href='/soft'>I'm Soft</a>.")
+    }
 }
